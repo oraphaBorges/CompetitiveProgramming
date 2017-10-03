0ba1c9d851ead7158b37f2e10633f3d950a18ab2
Raphael Borges <rah-borges@hotmail.com>
2017-10-03 08:57:35 -0300

    C and C++ codes
    On this directory, will contains the problems solved in C or C++.

---
+++ C | C++/URI1024.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char txt[1123],txt2;
+	int i, n, j, len, k;
+	
+	scanf("%d ",&n);
+	for (i = 0 ; i < n ; i++){
+		fgets(txt, 1123, stdin);
+		len = strlen(txt)-1;
+		for (j = 0; j <= len; j++){
+			if((txt[j] >= 'A' && txt[j] <= 'Z') || (txt[j] >= 'a' && txt[j] <= 'z')){
+				txt[j] += 3;
+			}
+		}
+		for (k = 0, j = len-1; k < j ; k++, j--){
+			txt2=txt[k];
+			txt[k]=txt[j];
+			txt[j]=txt2;
+			}
+		for (j = (len/2) ; j < len ; j++){
+			txt[j]--;
+			}
+		printf("%s", txt);
+	}
+	return 0;
+}
