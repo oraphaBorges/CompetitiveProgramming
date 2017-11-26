42170eb47e5462e739e80c97e7022b3a8e17bd08
ORaphaBorges <rah-borges@homail.com>
2017-11-25 23:20:12 -0200

    add the comment header

--- C | C++/URI1024.c
+++ C | C++/URI1024.c
@@ -1,10 +1,14 @@
+/*********************************************************************
+*	URI problem: 1024                                                  *
+* url: https://www.urionlinejudge.com.br/judge/en/problems/view/1024 *
+**********************************************************************/
 #include <stdio.h>
 #include <string.h>
 
 int main(){
 	char txt[1123],txt2;
 	int i, n, j, len, k;
-	
+
 	scanf("%d ",&n);
 	for (i = 0 ; i < n ; i++){
 		fgets(txt, 1123, stdin);
